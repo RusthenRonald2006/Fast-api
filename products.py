12eccbaa8452c178335b8bd4e4e2a1befd09d01d
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-25 15:21:14 -0300

    criando banco de dados fake

--- products.py
+++ products.py
@@ -1,1 +1,7 @@
 from pydantic import BaseModel
+
+#Em resumo, o BaseModel do Pydantic define a estrutura dos dados e garante que as informações que você recebe estejam no formato certo.
+
+class Product(BaseModel):
+    name: str
+    price: float
